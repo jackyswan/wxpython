ca4a8c1948c2ed597322c519424661b3a0a65527
jackyswan <jackyswan@foxmail.com>
2017-12-02 21:00:10 +0800

    Add Simple Editor.

--- wx_app.py
+++ wx_app.py
@@ -1,15 +1,40 @@
 import wx
+def load(event):
+	file = open(filename.GetValue())
+	contents.SetValue(file.read())
+	file.close()
+
+def save(event):
+	file = open(filename.GetValue(), 'w')
+	file.write(contents.GetValue())
+	file.close()
 
 app = wx.App()
-win = wx.Frame(None, title = "", size = (410, 335))
-win.Show()
+win = wx.Frame(None, title = "Simple Editor", size = (410, 335))
 
-loadButton = wx.Button(win, label = 'Open',
+bkg = wx.Panel(win)
+
+loadButton = wx.Button(bkg, label = 'Open',
 					pos = (255, 5), size = (80, 25))
 
-saveButton = wx.Button(win, label = 'Close',
-					pos = (315, 5), size = (80, 25))
+loadButton.Bind(wx.EVT_BUTTON, load)
 
-filename = wx.TextCtrl(win, pos = (5, 5), size = (210, 25))
+saveButton = wx.Button(bkg, label = 'Close',)
+saveButton.Bind(wx.Button, save)
 
-contents = wx.TextCtrl(win, pos = (5, 35))
+filename = wx.TextCtrl(bkg)
+
+contents = wx.TextCtrl(bkg, style = wx.TE_MULTILINE | wx.HSCROLL)
+
+hbox = wx.BoxSizer()
+hbox.Add(filename, proportion = 1, flag = wx.EXPEND)
+hbox.Add(loadButton, proportion = 0, flag = wx.LEFT, border = 5)
+hbox.Add(saveButton, proportion = 0, flag = ex.LEFT, border = 5)
+
+vbox = wx.BoxSizer(wx.VERTICAL)
+vbox.Add(hbox, proportion = 0, flag = wx.EXPEND | wx.ALL, border = 5)
+vbox.Add(contents, proportion = 1, 
+		flag = wx.EXPEND | wx.LEFT | wx.BOTTOM | wx.RIGHT, border = 5)
+
+bkg.SetSizer(vbox)
+win.Show()
